f667e4f6f56fad68fa5be07ba010b0cf540b055f
Ngo The Dong <ngothedong97@gmail.com>
2018-09-26 00:35:56 +0700

    First commit :))

---
+++ src/GameWindow.java
@@ -0,0 +1,75 @@
+import base.KeyEventPress;
+import tklibs.SpriteUtils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+
+public class GameWindow extends JFrame {
+    GameCanvas canvas;
+
+    public GameWindow() {
+        //setup window
+        this.setSize(800, 600);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setupEventListtener();
+        //init game
+        this.canvas = new GameCanvas();
+        this.add(canvas);
+
+        this.setVisible(true);
+    }
+
+    private void setupEventListtener() {
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_W) {
+                    KeyEventPress.isUpPress = true;
+                }
+                if(e.getKeyCode() == KeyEvent.VK_S) {
+                    KeyEventPress.isDownPress = true;
+                }
+                if(e.getKeyCode() == KeyEvent.VK_A) {
+                    KeyEventPress.isLeftPress = true;
+                }
+                if(e.getKeyCode() == KeyEvent.VK_D) {
+                    KeyEventPress.isRightPress = true;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_W) {
+                    KeyEventPress.isUpPress = false;
+                }
+                if(e.getKeyCode() == KeyEvent.VK_S) {
+                    KeyEventPress.isDownPress = false;
+                }
+                if(e.getKeyCode() == KeyEvent.VK_A) {
+                    KeyEventPress.isLeftPress = false;
+                }
+                if(e.getKeyCode() == KeyEvent.VK_D) {
+                    KeyEventPress.isRightPress = false;
+                }
+            }
+
+        });
+    }
+
+    void gameLoop() {
+        long delay = 1000 / 60;
+        long lastTime = 0;
+        while(true) {
+            long currentTime = System.currentTimeMillis();
+            if(currentTime - lastTime > delay) {
+                canvas.run();
+                this.repaint();
+                lastTime = currentTime;
+            }
+        }
+    }
+}
